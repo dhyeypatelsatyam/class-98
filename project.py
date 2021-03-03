ab13e3cfcf600d07ab44fbcc5ea37b0e0b7090dc
dhyeypatelsatyam <62635256+dhyeypatelsatyam@users.noreply.github.com>
2021-03-03 08:54:27 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,18 @@
+#def swapap():
+file1=input("enter the first file")
+file2=input("enter the second file")
+
+with open(file1, 'r') as a:
+    data1=a.read()
+
+with open(file2, 'r') as b:
+    data2=b.read()
+
+
+with open(file1, 'w') as a:
+    a.write(data2)
+
+with open(file2, 'w') as b:
+    b.write(data1)
+
+#swapap()
